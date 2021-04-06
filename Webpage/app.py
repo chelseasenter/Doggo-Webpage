03b6d72642a1e34886407ee5c9de541ef2b07792
BrandyB <blbst36@hotmail.com>
2021-04-05 21:09:40 -0400

    Commit to save changes

--- Webpage/app.py
+++ Webpage/app.py
@@ -11,10 +11,19 @@ Base = automap_base()
 
 Base.prepare(engine, reflect=True)
 temper = Base.classes.temperament
+breed_temper = Base.classes.breed_temperament
 
-
-@app.route("/")
+@app.routs("/")
 def welcome():
+    return(
+        f"Welcome to the doggo apis!"
+        f"If you are here without using the doggo page, shame, shame!!"
+        f"Here are the apis available:"
+        f"    /temperaments returns only the temperaments"
+        f"    /perfectPupper returns pups that contain specific temperaments"
+    )
+@app.route("/temperaments")
+def temperaments():
     session = Session(engine)
     all_temps = []
     results = session.query(temper.temperament_id, temper.temperament_name).all()
@@ -24,8 +33,17 @@ def welcome():
         temps["name"] = name
         temps["id"] = id
         all_temps.append(temps)
-
+    # Can be used to return only the name or name and id
+    session.close()
     return jsonify(temper_name)
+
+@app.route("/perfectPupper")
+def searchpups():
+    session = Session(engine)
+    puppers = session.query(breed_temper.breed).filter(breed_temper.temperament.in_([0,1,2,3]))
+    return jsonify(puppers)
+
+
 
 if __name__ == "__main__":
     app.run(debug=True)
